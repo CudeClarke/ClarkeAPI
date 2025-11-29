070c86e9b45ab2ebda663306b2acaaa7a2e4e6fd
SantyMG03 <santymorenog2003@gmail.com>
2025-11-29 17:12:22 +0100

    Refactor factory method (#4)

--- src/main/java/DB/iDatabase.java
+++ src/main/java/DB/iDatabase.java
@@ -13,4 +13,23 @@ public interface iDatabase {
      * Metodo para manejar la logica de dexconexion
      */
     void disconnect();
+
+    /**
+     * Crea una instancia de la BD solicitada.
+     * @param tipo "MySQL" o "MariaDB"
+     * @return Una instancia de iDatabase lista para usa
+     */
+    static iDatabase create(String tipo) {
+        if (tipo == null) return null;
+
+        return switch (tipo.toLowerCase()) {
+            case "mysql" -> new MySQLConnection();
+            case "mariadb" -> new MariaDBConnection();
+            // Aqui se agregan las que bd que hagan falta
+            default -> {
+                System.err.println("Error: " + tipo + " non supported");
+                yield null;
+            }
+        };
+    }
 }
