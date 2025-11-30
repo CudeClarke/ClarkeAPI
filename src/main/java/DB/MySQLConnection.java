26c4ed2a63a373f2d183636fb67f74f93a1e6a71
IvanBP-UMA <163024495+IvanBP-UMA@users.noreply.github.com>
2025-11-30 18:47:48 +0100

    Feature/add-db-dump (#10)
    * Added dump to root directory and added dependency to correctly access DB. Also changed query in UsuarioDAOMySSQL

--- src/main/java/DB/MySQLConnection.java
+++ src/main/java/DB/MySQLConnection.java
@@ -6,15 +6,17 @@ import java.sql.SQLException;
 
 public class MySQLConnection implements iDatabase{
     private Connection connection;
+    private final String url = "jdbc:mysql://localhost:3306/ClarkeDB";
+    private final String user = "clarke";
+    private final String password = "cudeca";
 
     @Override
     public Connection getConnection() {
         try {
-            String url = "jdbc:mysql://localhost:3306/clarkeca";
-            String user = "user_mysql";
-            String password = "pwd";
-            this.connection = DriverManager.getConnection(url,user,password);
-            System.out.println("Connected to MySQL successfully");
+            if(this.connection == null) {
+                this.connection = DriverManager.getConnection(url, user, password);
+                System.out.println("Connected to MySQL successfully");
+            }
             return this.connection;
         } catch (SQLException e) {
             System.err.println("MySQL failed to establish connection:  " + e.getMessage());
