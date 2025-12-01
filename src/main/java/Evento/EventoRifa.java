ceea4da89448c9750c7e3188b463ff10a5db4262
SantyMG03 <santymorenog2003@gmail.com>
2025-12-01 17:29:57 +0100

    Modificadas las clases de Evento

--- src/main/java/Evento/EventoRifa.java
+++ src/main/java/Evento/EventoRifa.java
@@ -6,9 +6,28 @@ import java.util.List;
 public class EventoRifa extends EventoAbstracto {
     private List<Integer> premios;
 
-    public EventoRifa(String nombre, int objetivoRecaudacion, int aforo) {
+    public EventoRifa(String nombre, int objetivoRecaudacion, int aforo, List<Integer> premios) {
         super(nombre, objetivoRecaudacion, aforo);
-        this.premios = new ArrayList<>();
+        this.premios = List.copyOf(premios);
+    }
+
+    public List<Integer> getPremios() {
+        return premios;
+    }
+
+    public void setPremios(List<Integer> premios) {
+        this.premios = List.copyOf(premios);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Rifa [");
+        for(int i = 0; i < premios.size(); i++){
+            sb.append(premios.get(i));
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
 }
