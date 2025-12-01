941bf1c411a294270946bdf28ec3e89a17ffb92d
aridpaquito <35864767+aridpaquito@users.noreply.github.com>
2025-12-01 19:41:08 +0100

    feat:Created EventoDAO and updated the DB. (#14)
    * Created EventoDAO and updated the DB
    
    * Changed EventoAbstracto to iEvento in methods
    
    * Changed EventoAbstracto to iEvento in methods

---
+++ src/main/java/DB/iEventoDAO.java
@@ -0,0 +1,13 @@
+package DB;
+
+import Evento.*;
+import java.util.List;
+import java.util.Map;
+
+public interface iEventoDAO {
+    iEvento searchByName(String nombre);
+    List<iEvento> searchByTag(String tag);
+    List<iEvento> searchByPatrocinador(String patrocinador);
+    List<iEvento> getAllEventos();
+    Map<String, Integer> countEntradasPorEvento(int idEvento);
+}
