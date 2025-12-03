e51b0bf58d1697a31a2bed06a6b74f30e1b84827
IvanBP-UMA <163024495+IvanBP-UMA@users.noreply.github.com>
2025-12-03 17:17:38 +0100

    Removed id attribute from Ticket classes and modified factories accordingly (also asiento in TicketConcierto is now a String) (#26)

--- src/main/java/Datos/Ticket/Ticket.java
+++ src/main/java/Datos/Ticket/Ticket.java
@@ -5,12 +5,10 @@ import Datos.Usuario.iUsuario;
 public class Ticket implements iTicket {
     private iUsuario usuario;
     private String dniAsistente;
-    private int id;
 
-    public Ticket (iUsuario usuario, String dniBeneficiario, int id) {
+    public Ticket (iUsuario usuario, String dniBeneficiario) {
         this.usuario = usuario;
         this.dniAsistente = dniBeneficiario;
-        this.id = id;
     }
 
     public iUsuario getUsuario() {
@@ -29,15 +27,7 @@ public class Ticket implements iTicket {
         this.dniAsistente = dniAsistente;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String toString(){
-        return "Ticket [Nombre=" + usuario.toString() + ", DNI=" + dniAsistente + ", ID=" + id + "]";
+        return "Ticket [Nombre=" + usuario.toString() + ", DNI=" + dniAsistente + "]";
     }
 }
--- src/main/java/Datos/Ticket/TicketFactory.java
+++ src/main/java/Datos/Ticket/TicketFactory.java
@@ -3,5 +3,5 @@ package Datos.Ticket;
 import Datos.Usuario.iUsuario;
 
 public abstract class TicketFactory {
-    public abstract iTicket createTicket(iUsuario usuario, String dniBeneficiario, int id, int info);
+    public abstract iTicket createTicket(iUsuario usuario, String dniBeneficiario, String info);
 }
