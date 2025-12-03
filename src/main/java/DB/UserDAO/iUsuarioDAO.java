383d5d92e1133afaf1475b8c6423fc3d602f88c8
SantyMG03 <santymorenog2003@gmail.com>
2025-12-03 19:47:12 +0100

    Refactor: UsuarioDAO modified to match previous changes (#29)
    * Modified UsuarioRegistrado to fit the db
    
    * Refactorized UsuarioDAO and iUsuario to match

--- src/main/java/DB/UserDAO/iUsuarioDAO.java
+++ src/main/java/DB/UserDAO/iUsuarioDAO.java
@@ -5,10 +5,9 @@ import Datos.Usuario.*;
 import java.util.List;
 
 public interface iUsuarioDAO {
-    List<iUsuario> getAllUsuarios();
-    iUsuario searchByDni(String DNI);
+    List<UsuarioBase> listAllUsuarios();
+    UsuarioBase searchByDni(String DNI);
     boolean registerUsuarioBase(UsuarioBase usuario);
-    boolean registerUsuarioRegistrado(UsuarioRegistrado usuario);
     boolean updateUsuario(iUsuario usuario);
     boolean deleteUsuario(String DNI);
 }
--- src/main/java/Datos/Usuario/UsuarioRegistrado.java
+++ src/main/java/Datos/Usuario/UsuarioRegistrado.java
@@ -2,15 +2,15 @@ package Datos.Usuario;
 
 public class UsuarioRegistrado extends UsuarioBase {
     private String direccion;
-    private int tlf;
+    private String tlf;
 
-    public UsuarioRegistrado(String nombre, String apellido, String email, String dni, boolean spam, String direccion, int tlf) {
+    public UsuarioRegistrado(String nombre, String apellido, String email, String dni, boolean spam, String direccion, String tlf) {
         super(nombre, apellido, email, dni, spam);
         this.direccion = direccion;
         this.tlf = tlf;
     }
 
-    public UsuarioRegistrado(String nombre, String apellido, String email, String dni, String direccion, int tlf) {
+    public UsuarioRegistrado(String nombre, String apellido, String email, String dni, String direccion, String tlf) {
         super(nombre, apellido, email, dni);
         this.direccion = direccion;
         this.tlf = tlf;
@@ -20,15 +20,15 @@ public class UsuarioRegistrado extends UsuarioBase {
         return direccion;
     }
 
-    public int getTlf() {
-        return tlf;
-    }
-
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
-    public void setTlf(int tlf) {
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
         this.tlf = tlf;
     }
 
