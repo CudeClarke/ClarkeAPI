523cb158e6707a3428ee8434c5fd6ff7e31566fa
JulianDelaMaza <162130539+JulianDelaMaza@users.noreply.github.com>
2025-12-04 15:46:35 +0100

    Refactored TicketDAOMySQL. Need fixation in the two search methods because of the user. Also need to be discused if informacion is a parameter for registerTicket method (#31)

--- src/main/java/DB/TicketDAO/iTicketDAO.java
+++ src/main/java/DB/TicketDAO/iTicketDAO.java
@@ -6,6 +6,6 @@ import java.util.List;
 public interface iTicketDAO {
     List<iTicket> searchByUser(String dni);
     List<iTicket> searchByEntrada(int idEntrada);
-    boolean registerTicket(iTicket ticket, String dniComprador, int idEntrada);
+    boolean registerTicket(iTicket ticket, String dniComprador, int idEntrada, String informacion);
     boolean deleteTicket(String id);
 }
