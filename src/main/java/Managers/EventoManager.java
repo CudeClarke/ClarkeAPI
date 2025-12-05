9370272043acc2663db6ddb986f49498c5e3b100
IvanBP-UMA <163024495+IvanBP-UMA@users.noreply.github.com>
2025-12-05 12:17:25 +0100

    Feature/create evento manager (#40)
    * Created UserManager class, modified UserHandlers accordingly and slightly modified types in iUsuarioDAO and its implementations
    
    * Created EventoManager and refactorized EventoHandlers accordingly. Slight modification in iEventoDAO interface to ease the implementation

---
+++ src/main/java/Managers/EventoManager.java
@@ -0,0 +1,71 @@
+package Managers;
+
+import DB.iDatabaseAccessFactory;
+import DB.EventoDAO.iEventoDAO;
+import Datos.Evento.*;
+
+import java.util.List;
+
+public class EventoManager {
+    private iEventoDAO eventoDAO;
+
+    public EventoManager(iDatabaseAccessFactory factory){
+        eventoDAO = factory.getEventoDAO();
+    }
+
+    private boolean validEvent(iEvento evento){return (evento.getNombre() != null && !evento.getNombre().isBlank() && evento.getDate() != null);}
+    private int getEventType(iEvento evento){
+        return switch (evento) {
+            case EventoCarrera _ -> 1;
+            case EventoRifa _ -> 2;
+            case EventoConcierto _ -> 3;
+            default -> 4;
+        };
+    }
+
+    public List<iEvento> getAllEventos(){
+        return eventoDAO.getAllEventos();
+    }
+
+    public iEvento searchByName(String name){
+        return eventoDAO.searchByName(name);
+    }
+
+    public List<iEvento> searchByTag(String tag){
+        return eventoDAO.searchByTag(tag);
+    }
+
+    public List<iEvento> searchByPatrocinador(String patrocinador){
+        return eventoDAO.searchByPatrocinador(patrocinador);
+    }
+
+    public boolean registerEvento(iEvento evento){
+        boolean res = false;
+        if (validEvent(evento)){
+            res = eventoDAO.registerEvento(evento, getEventType(evento));
+        }
+        return res;
+    }
+
+    public boolean updateEvento(iEvento evento){
+        boolean res = false;
+        if (validEvent(evento)){
+            int id = eventoDAO.getID(evento);
+            if (id > 0){
+                res = eventoDAO.updateEvento(id, evento);
+            }
+        }
+        return res;
+    }
+
+    public boolean deleteEvento(iEvento evento){
+        boolean res = false;
+        if (validEvent(evento)){
+            int id = eventoDAO.getID(evento);
+            if (id > 0){
+                res = eventoDAO.deleteEvento(id);
+            }
+        }
+        return res;
+    }
+}
