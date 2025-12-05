3ea7f016d2ec5dd6f7fbe310cd75d43dd1da115c
aridpaquito <35864767+aridpaquito@users.noreply.github.com>
2025-12-05 16:28:21 +0100

    Added getInformacion, changed attribute types and added atribute recaudacion on every factory (#43)

--- src/main/java/Datos/Evento/EventoCarreraFactory.java
+++ src/main/java/Datos/Evento/EventoCarreraFactory.java
@@ -4,9 +4,8 @@ import java.sql.Date;
 
 public class EventoCarreraFactory extends EventoFactory{
     @Override
-    public iEvento createEvento(String nombre, String ubicacion, int objetivoRecaudacion, String descripcion, Date date, String url, String informacion) {
-        // TODO: Convertir informacion_extra a los atributos necesarios de la clase concreta
-        // El código se deja así ante el inminente refactor de las clases de datos
-        return new EventoCarrera(nombre, ubicacion, objetivoRecaudacion, descripcion, date, url, 10);
+    public iEvento createEvento(String nombre, String ubicacion, int recaudacion, int objetivoRecaudacion, String descripcion, Date date, String url, String informacion) {
+        int recorrido = Integer.parseInt(informacion);
+        return new EventoCarrera(nombre, ubicacion, recaudacion, objetivoRecaudacion, descripcion, date, url, recorrido);
     }
 }
--- src/main/java/Datos/Evento/EventoFactory.java
+++ src/main/java/Datos/Evento/EventoFactory.java
@@ -3,5 +3,5 @@ package Datos.Evento;
 import java.sql.Date;
 
 public abstract class EventoFactory {
-    public abstract iEvento createEvento(String nombre, String ubicacion, int objetivoRecaudacion, String descripcion, Date date, String url, String informacion);
+    public abstract iEvento createEvento(String nombre, String ubicacion, int recaudacion, int objetivoRecaudacion, String descripcion, Date date, String url, String informacion);
 }
