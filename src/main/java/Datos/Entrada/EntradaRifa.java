16ccc619b2fece7933df3bc6e83a04177967a465
IvanBP-UMA <163024495+IvanBP-UMA@users.noreply.github.com>
2025-12-05 11:55:25 +0100

    Renamed informacion to nombre in eventos and created descripcion attribute (#38)

--- src/main/java/Datos/Entrada/EntradaRifa.java
+++ src/main/java/Datos/Entrada/EntradaRifa.java
@@ -2,8 +2,8 @@ package Datos.Entrada;
 
 public class EntradaRifa extends Entrada {
 
-    public EntradaRifa(int subAforo, float precio) {
-        super(subAforo, precio);
+    public EntradaRifa(int subAforo, float precio, String nombre, String descripcion,  int seccion) {
+        super(subAforo, precio, nombre, descripcion);
     }
 
     public String toString(){
