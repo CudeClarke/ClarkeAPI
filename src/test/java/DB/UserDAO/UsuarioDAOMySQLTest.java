ec9b261da18333c0168e4fccfe0c9a6a9aea7e87
SantyMG03 <santymorenog2003@gmail.com>
2025-12-06 16:42:23 +0100

    WIP: TestRegister Search

--- src/test/java/DB/UserDAO/UsuarioDAOMySQLTest.java
+++ src/test/java/DB/UserDAO/UsuarioDAOMySQLTest.java
@@ -1,8 +1,13 @@
 package DB.UserDAO;
 
 import DB.MySQLConnection;
+import Datos.Usuario.UsuarioBase;
+import Datos.Usuario.UsuarioRegistrado;
+import Datos.Usuario.iUsuario;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -38,5 +43,31 @@ class UsuarioDAOMySQLTest {
             psUser.setString(1, DNI_TEST);
             psUser.executeUpdate();
         }
+    }
+
+    @Test
+    @DisplayName("Registrar y Buscar Usuario Registrado")
+    void testRegisterSearch() {
+        // GIVEN
+        UsuarioRegistrado nuevo = new UsuarioRegistrado(
+                "TestNombre", "TestApellido", "test@junit.com", DNI_TEST, true,
+                "Calle Test 123", "600000000"
+        );
+
+        // WHEN
+        boolean registrado = usuarioDAO.registerUsuario(nuevo);
+
+        // THEN
+        assertTrue(registrado, "El registro debería devolver true");
+
+        iUsuario recuperado = usuarioDAO.searchByDni(DNI_TEST);
+        assertNotNull(recuperado, "El usuario recuperado no debe ser null");
+
+        // Verificamos que recupera la subclase correcta
+        assertInstanceOf(UsuarioRegistrado.class, recuperado, "Debe ser instancia de UsuarioRegistrado");
+
+        UsuarioRegistrado ur = (UsuarioRegistrado) recuperado;
+        assertEquals("Calle Test 123", ur.getDireccion());
+        assertEquals("600000000", ur.getTlf());
     }
 }
