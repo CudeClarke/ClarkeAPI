291f24297d7a343f69599a01490aa494127601dd
aridpaquito <35864767+aridpaquito@users.noreply.github.com>
2025-12-09 13:40:01 +0100

    Feature/new methods EventoDAOMySQL (#61)
    * Implemented getPatrocinadores and getTag in EventoDAOMySQL
    
    * Fixed query in getPatrocinadores

--- src/main/java/Datos/Evento/Patrocinador.java
+++ src/main/java/Datos/Evento/Patrocinador.java
@@ -5,12 +5,10 @@ import java.util.Objects;
 public class Patrocinador {
     private String nombre;
     private String logo;
-    private String web;
 
-    public Patrocinador(String nombre, String logo, String web) {
+    public Patrocinador(String nombre, String logo) {
         this.nombre = nombre;
         this.logo = logo;
-        this.web = web;
     }
 
     public String getNombre() {
@@ -29,28 +27,20 @@ public class Patrocinador {
         this.logo = logo;
     }
 
-    public String getWeb() {
-        return web;
-    }
-
-    public void setWeb(String web) {
-        this.web = web;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Patrocinador that = (Patrocinador) o;
-        return Objects.equals(nombre, that.nombre) && Objects.equals(logo, that.logo) && Objects.equals(web, that.web);
+        return Objects.equals(nombre, that.nombre) && Objects.equals(logo, that.logo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nombre, logo, web);
+        return Objects.hash(nombre, logo);
     }
 
     @Override
     public String toString() {
-        return "Patrocinador [nombre=" + nombre + ", logo=" + logo + ", web=" + web + "]";
+        return "Patrocinador [nombre=" + nombre + ", logo=" + logo + "]";
     }
 }
