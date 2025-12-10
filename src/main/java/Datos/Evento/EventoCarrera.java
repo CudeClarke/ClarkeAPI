e893a590cfd9f1a10e6f5f49ac51d8ad5e80e85e
Arid <aridpaquito@gmail.com>
2025-12-10 16:17:08 +0100

    Refactorized date in evento into a String

--- src/main/java/Datos/Evento/EventoCarrera.java
+++ src/main/java/Datos/Evento/EventoCarrera.java
@@ -1,21 +1,20 @@
 package Datos.Evento;
 
-import java.sql.Date;
 
 public class EventoCarrera extends Evento {
     private int recorrido;
 
-    public EventoCarrera(String nombre, String ubicacion, int objetivoRecaudacion, String descripcion, Date date, String url, int recorrido) {
+    public EventoCarrera(String nombre, String ubicacion, int objetivoRecaudacion, String descripcion, String date, String url, int recorrido) {
         super(nombre, ubicacion, objetivoRecaudacion, descripcion, date, url);
         this.recorrido = recorrido;
     }
 
-    public EventoCarrera(String nombre, String ubicacion, int recaudacion, int objetivoRecaudacion, String descripcion, Date date, String url, int recorrido) {
+    public EventoCarrera(String nombre, String ubicacion, int recaudacion, int objetivoRecaudacion, String descripcion, String date, String url, int recorrido) {
         super(nombre, ubicacion, recaudacion, objetivoRecaudacion, descripcion, date, url);
         this.recorrido = recorrido;
     }
 
-    public EventoCarrera(String nombre, String ubicacion, int recaudacion, int objetivoRecaudacion, String descripcion, Date date, String url, int ID, int recorrido) {
+    public EventoCarrera(String nombre, String ubicacion, int recaudacion, int objetivoRecaudacion, String descripcion, String date, String url, int ID, int recorrido) {
         super(nombre, ubicacion, recaudacion, objetivoRecaudacion, descripcion, date, url, ID);
         this.recorrido = recorrido;
     }
