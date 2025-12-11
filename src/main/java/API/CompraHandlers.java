cb76b5590efe045c0c04b2a77e735e01e53ba7f1
SantyMG03 <santymorenog2003@gmail.com>
2025-12-11 11:32:05 +0100

    Small change to start testing

--- src/main/java/API/CompraHandlers.java
+++ src/main/java/API/CompraHandlers.java
@@ -14,9 +14,9 @@ import Managers.CompraManager;
 import Managers.Transaction;
 
 public class CompraHandlers {
-    private static final CompraManager compraManager = new CompraManager(MySQLAccessFactory.getInstance());
-    private static final EventoManager eventoManager = new EventoManager(MySQLAccessFactory.getInstance());
-    private static final UserManager userManager = new UserManager(MySQLAccessFactory.getInstance());
+    private static CompraManager compraManager = new CompraManager(MySQLAccessFactory.getInstance());
+    private static EventoManager eventoManager = new EventoManager(MySQLAccessFactory.getInstance());
+    private static UserManager userManager = new UserManager(MySQLAccessFactory.getInstance());
     private static final String ID_EVENTO = "idEvento";
     private static final String ID_ENTRADA = "idEntrada";
     private static final String AMOUNT = "amount";
